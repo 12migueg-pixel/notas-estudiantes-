066acb7eff42aa0e0866442d74eda8dfd7825b0e
miguel <12migueg@gmail.com>
2026-02-20 17:38:29 -0500

    notas

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args){
+
+        estudiante e1 = new estudiante("sofia ", 3.5, 3.9, 4.5);
+        e1.mostrarInformacion();
+
+        System.out.println(e1.calcularPromedio());
+
+        System.out.println(e1.estaAprovado(3.0));
+
+        System.out.println("el promedio es: " + e1.calcularPromedio());
+
+
+        estudiante e2 = new estudiante("laura");
+        e2.asignarNotas(4.5,2.9,1.5);
+
+        e2.mostrarInformacion();
+        System.out.println("el promedio es: " + e2.calcularPromedio());
+
+        System.out.println(e2.estaAprovado(3.0));
+
+
+
+
+
+
+
+    }
+}
